c21e4f38dc83e4ca0b2df045cab134adb5a4dc19
PiotrZak <piotrzak77@gmail.com>
2021-08-06 20:52:32 +0200

    EnemyLocation

---
+++ Game/EnemyLocation.cs
@@ -0,0 +1,12 @@
+using Game.Engine;
+
+namespace Game
+{
+    public class Enemy
+    {
+        public static void CheckForEnemy(Location newLocation)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
